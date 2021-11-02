547d3eaf25d684e778c343e64dd9db523d7d366c
David Gilpin <dgilpin@bmchs.org>
2021-11-02 14:50:14 -0500

    latest version

--- Game.py
+++ Game.py
@@ -3,11 +3,35 @@ Car racing game
 '''
 import pygame, sys
 from pygame.locals import *
+import random
 
 pygame.init()
 
-DISPLAYSURF = pygame.display.set_mode((300, 300))
+# frames per second
+FPS = 60
+clock = pygame.time.Clock()
+
+# set up color variables
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN  = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# setup 300x300 display
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+DISPLAYSURF.fill(WHITE)
+pygame.display.set_caption("Game")
+
+# classes
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        self.image = pygame.image.load("Enemy.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
 # game loop begins
 while True:
     # events
@@ -17,8 +41,9 @@ while True:
             sys.exit()
 
     # data changes
-    pygame.draw.circle(DISPLAYSURF, BLACK, (200, 50), 30)
 
     # update the display
     pygame.display.update()
 
+    # control FPS
+    clock.tick(FPS)
